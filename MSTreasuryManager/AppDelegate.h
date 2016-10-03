56c47004b0ce9479dc3fc626a9674872448ce968
邓利文 <dengliwen@dengliwendeMac-mini.local>
2016-10-03 09:36:20 +0800

    Initial Commit

---
+++ MSTreasuryManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSTreasuryManager
+//
+//  Created by 邓利文 on 2016/10/3.
+//  Copyright © 2016年 邓利文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
